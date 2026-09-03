6a6db0c599f899a20b0435d41fe75d40b37f5d65
MatheusCruz08 <matheus.dcruz@sptech.school>
2026-09-03 20:42:16 -0300

    Novo script de python com discretização de dados

--- captura.py
+++ captura.py
@@ -2,6 +2,8 @@ import psutil as p
 import time as t
 from datetime import datetime
 import mysql.connector
+from rich import print
+
 
 def banco(N1, N2, N3, N4):
 
@@ -42,21 +44,32 @@ memoria_utilizada = 0
 porcentagem_de_disco = 0
 espaco_total = 0
 espaco_livre = 0
-espaco_utilizado = 0 
+espaco_utilizado = 0
 
 
 def CPU():
 
-    # Análise CPU
     print('\n')
-
-    print("Porcentagem de CPU usada:")
     porcentagem_de_uso_cpu = p.cpu_percent(interval=0.1)
-    print(porcentagem_de_uso_cpu)
-
-    print("Frequência da CPU")
     frequencia = p.cpu_freq().current
-    print(frequencia)
+
+    if porcentagem_de_uso_cpu >= 80 :
+        print(f"Alerta o uso da sua CPU está em: [bold red]{porcentagem_de_uso_cpu}% [/bold red]")   
+
+    elif porcentagem_de_uso_cpu >= 2 :
+        print(f"Alerta o uso da sua CPU está em: [bold yellow]{porcentagem_de_uso_cpu}% [/bold yellow]")  
+
+    else : 
+        print(f"Porcentagem de uso da CPU: [blue]{porcentagem_de_uso_cpu}%[/blue]")
+
+    if frequencia >= 1700 :
+        print(f"Alerta a frequência da sua CPU está em: [bold red]{frequencia}Hz [/bold red]")   
+
+    elif frequencia >= 1500 :
+        print(f"Alerta a frequência da sua CPU está em: [bold yellow]{porcentagem_de_uso_cpu}Hz [/bold yellow]")  
+
+    else :
+        print(f"A frequência da sua CPU está em: [blue]{frequencia}Hz[/blue]")
 
     print('\n')
 
@@ -65,25 +78,46 @@ def CPU():
 
 
 def RAM() :
-    # Análise memória RAM
-        
-    print("Memória RAM utilizada:") 
+         
     porcentagem_de_uso_ram = p.virtual_memory().percent
-    print(porcentagem_de_uso_ram)
-    
-    print("Memória RAM total:") 
     memoria_total = round(p.virtual_memory().total / (1024**3))
-    print(memoria_total)
-    
-    print("Memória RAM disponível:")
     memoria_disponivel = round(p.virtual_memory().available / (1024**3))
-    print(memoria_disponivel)
-    
-    print("Memória RAM usada:")
     memoria_utilizada = round(p.virtual_memory().used / (1024**3))
-    print(memoria_utilizada)
+
+    if porcentagem_de_uso_ram >= 80 :
+        print(f"Alerta o uso da sua RAM está em: [bold red]{porcentagem_de_uso_ram}% [/bold red]")   
+
+    elif porcentagem_de_uso_ram >= 65 :
+        print(f"Alerta o uso da sua RAM está em: [bold yellow]{porcentagem_de_uso_cpu}% [/bold yellow]") 
+
+    else :
+        print(f"Porcentagem de uso da RAM: [blue]{porcentagem_de_uso_ram}%[/blue]")
+
     
+    print(f"[blue]{memoria_total}Gb[/blue]")
+
+
+    if memoria_disponivel < 7 :
+        print(f"Alerta você só tem: [bold red]{memoria_disponivel}Gb da sua RAM dísponivel [/bold red]")
+
+    elif memoria_disponivel <= 5.5 :
+        print(f"Alerta você só tem: [bold yellow]{memoria_disponivel}Gb da sua RAM dísponivel [/bold yellow]")
+
+    else :
+        print(f"[blue]{memoria_disponivel}Gb[/blue]")
+
+    if memoria_utilizada >= 7 :
+        print(f"Alerta você está usando: [bold red]{memoria_utilizada}Gb da sua RAM [/bold red]")
+
+    elif memoria_utilizada >= 5.5 :
+        print(f"Alerta você está usando: [bold yellow]{memoria_utilizada}Gb da sua RAM [/bold yellow]")   
+
+    else :
+        print(f"Gigabytes de uso da RAM: [blue]{memoria_utilizada}Gb[/blue]")
+
+
     print('\n')
+
 
     banco(porcentagem_de_uso_ram, 'Uso',7, '%')
     banco(memoria_total, 'Total', 7, 'Gb')
@@ -92,24 +126,44 @@ def RAM() :
 
 
 def Disco() :
-     # Análise Disco
-                         
-    print("Porcentagem do Disco usada:")
-    porcentagem_de_disco = p.disk_usage('C:\\').percent
-    print(porcentagem_de_disco)
-     
-    print("Disco total usado:")
-    espaco_total = round(p.disk_usage('C:\\').total / (1024 ** 3))
-    print(espaco_total)
-         
-    print("Parte livre do disco:")
-    espaco_livre = round(p.disk_usage('C:\\').free / (1024 ** 3))
-    print(espaco_livre)
-         
-    print("Parte usada do disco:") 
-    espaco_utilizado = round(p.disk_usage('C:\\').used / (1024 ** 3))
-    print(espaco_utilizado)
 
+    porcentagem_de_disco = p.disk_usage('C:\\').percent
+    espaco_total = round(p.disk_usage('C:\\').total / (1024 ** 3))
+    espaco_livre = round(p.disk_usage('C:\\').free / (1024 ** 3))
+    espaco_utilizado = round(p.disk_usage('C:\\').used / (1024 ** 3))
+
+    if porcentagem_de_disco >= 80 :
+        print(f"Alerta o uso do seu Disco está em: [bold red]{porcentagem_de_disco}% [/bold red]") 
+
+    elif porcentagem_de_disco >= 65 :
+        print(f"Alerta o uso do seu Disco está em: [bold yellow]{memoria_disponivel}% [/bold yellow]")  
+
+    else :
+        print(f"Porcentagem de uso do Disco: [blue]{porcentagem_de_disco}%[/blue]")
+
+    
+    print(f"Espaço total do seu Disco: [blue]{espaco_total}Gb[/blue]")
+
+    
+    if espaco_livre < 20 :
+        print(f"Alerta o uso do seu Disco está em: [bold red]{espaco_livre}Gb [/bold red]")
+
+    elif memoria_disponivel <= 45 :
+        print(f"Alerta o uso do seu Disco está em: [bold yellow]{memoria_disponivel}Gb [/bold yellow]") 
+
+    else :
+        print(f"Espaço livre do seu Disco: [blue]{espaco_livre}Gb[/blue]")
+    
+    if espaco_utilizado >= 220 :
+        print(f"Alerta você só tem [bold red]{espaco_total - espaco_utilizado}Gb do seu Disco dísponivel [/bold red]")
+
+    elif espaco_utilizado <= 5.5 :
+        print(f"Alerta você só tem: [bold yellow]{espaco_total - espaco_utilizado}Gb do seu Disco dísponivel [/bold yellow]") 
+
+    else :
+        print(f"Espaço do Disco que está sendo utilizado: [blue]{espaco_utilizado}Gb[/blue]")
+
+    
     banco(porcentagem_de_disco, 'Uso', 13, '%')
     banco(espaco_total, 'Total', 13, 'Gb')
     banco(espaco_livre, 'Disponível', 13, 'Gb')
---
+++ teste.py
@@ -0,0 +1,4 @@
+from rich import print
+
+print("[bold red]Texto em vermelho e negrito![/bold red]")
+print("[blue]Este texto é azul[/blue] e [green]este é verde[/green].")
